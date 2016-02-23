1ece7579d5a2319ae1a07989aa9ecc710c4a9833
Tiago Rodrigues <ttiago16@hotmail.com>
2016-02-23 14:12:55 +0000

    estudio_musica

--- estudio_musica/estudio_musica/Form1.cs
+++ estudio_musica/estudio_musica/Form1.cs
@@ -6,8 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using MySql.Data.MySqlClient;
+using System.Windows.Forms;
 
 namespace estudio_musica
 {
