51e4aebe7b03f3123ed0e683f4240382d5887027
Tiago Rodrigues <ttiago16@hotmail.com>
2016-03-02 09:13:16 +0000

    01.03.2016

---
+++ estudio_musica/estudio_musica/Leitor_video.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace estudio_musica
+{
+    partial class Leitor_video
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Leitor_video));
+            this.youtube_player = new AxShockwaveFlashObjects.AxShockwaveFlash();
+            this.button_voltar = new System.Windows.Forms.PictureBox();
+            this.button_sair = new System.Windows.Forms.PictureBox();
+            this.button_charli_xcx = new System.Windows.Forms.Button();
+            this.button_metalica = new System.Windows.Forms.Button();
+            this.button_richie_campbell = new System.Windows.Forms.Button();
+            this.button_eminem = new System.Windows.Forms.Button();
+            this.button_ed_sheeran = new System.Windows.Forms.Button();
+            this.button_imagine_dragons = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.youtube_player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_voltar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_sair)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // youtube_player
+            // 
+            this.youtube_player.Enabled = true;
+            this.youtube_player.Location = new System.Drawing.Point(125, 12);
+            this.youtube_player.Name = "youtube_player";
+            this.youtube_player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("youtube_player.OcxState")));
+            this.youtube_player.Size = new System.Drawing.Size(411, 204);
+            this.youtube_player.TabIndex = 0;
+            // 
+            // button_voltar
+            // 
+            this.button_voltar.BackColor = System.Drawing.Color.Transparent;
+            this.button_voltar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_voltar.Image = ((System.Drawing.Image)(resources.GetObject("button_voltar.Image")));
+            this.button_voltar.Location = new System.Drawing.Point(12, 281);
+            this.button_voltar.Name = "button_voltar";
+            this.button_voltar.Size = new System.Drawing.Size(65, 41);
+            this.button_voltar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.button_voltar.TabIndex = 14;
+            this.button_voltar.TabStop = false;
+            this.button_voltar.Click += new System.EventHandler(this.button_voltar_Click);
+            // 
+            // button_sair
+            // 
+            this.button_sair.BackColor = System.Drawing.Color.Transparent;
+            this.button_sair.Image = ((System.Drawing.Image)(resources.GetObject("button_sair.Image")));
+            this.button_sair.Location = new System.Drawing.Point(461, 281);
+            this.button_sair.Name = "button_sair";
+            this.button_sair.Size = new System.Drawing.Size(75, 41);
+            this.button_sair.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.button_sair.TabIndex = 15;
+            this.button_sair.TabStop = false;
+            this.button_sair.Click += new System.EventHandler(this.button_sair_Click);
+            // 
+            // button_charli_xcx
+            // 
+            this.button_charli_xcx.Location = new System.Drawing.Point(12, 12);
+            this.button_charli_xcx.Name = "button_charli_xcx";
+            this.button_charli_xcx.Size = new System.Drawing.Size(96, 23);
+            this.button_charli_xcx.TabIndex = 16;
+            this.button_charli_xcx.Text = "Charli XCX";
+            this.button_charli_xcx.UseVisualStyleBackColor = true;
+            this.button_charli_xcx.Click += new System.EventHandler(this.button_charli_xcx_Click);
+            // 
+            // button_metalica
+            // 
+            this.button_metalica.Location = new System.Drawing.Point(12, 41);
+            this.button_metalica.Name = "button_metalica";
+            this.button_metalica.Size = new System.Drawing.Size(96, 23);
+            this.button_metalica.TabIndex = 17;
+            this.button_metalica.Text = "Metalica";
+            this.button_metalica.UseVisualStyleBackColor = true;
+            this.button_metalica.Click += new System.EventHandler(this.button_metalica_Click);
+            // 
+            // button_richie_campbell
+            // 
+            this.button_richie_campbell.Location = new System.Drawing.Point(12, 70);
+            this.button_richie_campbell.Name = "button_richie_campbell";
+            this.button_richie_campbell.Size = new System.Drawing.Size(96, 23);
+            this.button_richie_campbell.TabIndex = 18;
+            this.button_richie_campbell.Text = "Richie Campbell";
+            this.button_richie_campbell.UseVisualStyleBackColor = true;
+            this.button_richie_campbell.Click += new System.EventHandler(this.button_richie_campbell_Click);
+            // 
+            // button_eminem
+            // 
+            this.button_eminem.Location = new System.Drawing.Point(12, 99);
+            this.button_eminem.Name = "button_eminem";
+            this.button_eminem.Size = new System.Drawing.Size(96, 23);
+            this.button_eminem.TabIndex = 19;
+            this.button_eminem.Text = "Eminem";
+            this.button_eminem.UseVisualStyleBackColor = true;
+            this.button_eminem.Click += new System.EventHandler(this.button_eminem_Click);
+            // 
+            // button_ed_sheeran
+            // 
+            this.button_ed_sheeran.Location = new System.Drawing.Point(12, 128);
+            this.button_ed_sheeran.Name = "button_ed_sheeran";
+            this.button_ed_sheeran.Size = new System.Drawing.Size(96, 23);
+            this.button_ed_sheeran.TabIndex = 20;
+            this.button_ed_sheeran.Text = "Ed Sheeran";
+            this.button_ed_sheeran.UseVisualStyleBackColor = true;
+            this.button_ed_sheeran.Click += new System.EventHandler(this.button_ed_sheeran_Click);
+            // 
+            // button_imagine_dragons
+            // 
+            this.button_imagine_dragons.Location = new System.Drawing.Point(12, 157);
+            this.button_imagine_dragons.Name = "button_imagine_dragons";
+            this.button_imagine_dragons.Size = new System.Drawing.Size(96, 23);
+            this.button_imagine_dragons.TabIndex = 21;
+            this.button_imagine_dragons.Text = "Imagine Dragons";
+            this.button_imagine_dragons.UseVisualStyleBackColor = true;
+            this.button_imagine_dragons.Click += new System.EventHandler(this.button_imagine_dragons_Click);
+            // 
+            // Leitor_video
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(548, 334);
+            this.Controls.Add(this.button_imagine_dragons);
+            this.Controls.Add(this.button_ed_sheeran);
+            this.Controls.Add(this.button_eminem);
+            this.Controls.Add(this.button_richie_campbell);
+            this.Controls.Add(this.button_metalica);
+            this.Controls.Add(this.button_charli_xcx);
+            this.Controls.Add(this.button_sair);
+            this.Controls.Add(this.button_voltar);
+            this.Controls.Add(this.youtube_player);
+            this.Name = "Leitor_video";
+            this.Text = "Leitor de Vídeo";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Leitor_video_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.youtube_player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_voltar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.button_sair)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxShockwaveFlashObjects.AxShockwaveFlash youtube_player;
+        private System.Windows.Forms.PictureBox button_voltar;
+        private System.Windows.Forms.PictureBox button_sair;
+        private System.Windows.Forms.Button button_charli_xcx;
+        private System.Windows.Forms.Button button_metalica;
+        private System.Windows.Forms.Button button_richie_campbell;
+        private System.Windows.Forms.Button button_eminem;
+        private System.Windows.Forms.Button button_ed_sheeran;
+        private System.Windows.Forms.Button button_imagine_dragons;
+    }
+}
