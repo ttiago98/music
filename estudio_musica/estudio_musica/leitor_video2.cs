8b08eaa56c89440ebc569d442d802ce56a9b3091
Tiago Rodrigues <ttiago16@hotmail.com>
2016-03-09 10:02:26 +0000

    9.3.16

---
+++ estudio_musica/estudio_musica/leitor_video2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace estudio_musica
+{
+    public partial class leitor_video2 : Form
+    {
+        public leitor_video2()
+        {
+            InitializeComponent();
+        }
+    }
+}
