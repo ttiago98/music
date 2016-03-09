d27567550e6a6c8fc154c17920380d1a8d029f8b
Tiago Rodrigues <ttiago16@hotmail.com>
2016-03-09 10:06:06 +0000

    9.3.16

--- estudio_musica/estudio_musica/leitor_video2.Designer.cs
+++ estudio_musica/estudio_musica/leitor_video2.Designer.cs
@@ -29,7 +29,66 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(leitor_video2));
+            this.youtube_player = new AxShockwaveFlashObjects.AxShockwaveFlash();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.youtube_player)).BeginInit();
             this.SuspendLayout();
+            // 
+            // youtube_player
+            // 
+            this.youtube_player.Enabled = true;
+            this.youtube_player.Location = new System.Drawing.Point(32, 27);
+            this.youtube_player.Name = "youtube_player";
+            this.youtube_player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("youtube_player.OcxState")));
+            this.youtube_player.Size = new System.Drawing.Size(491, 268);
+            this.youtube_player.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(550, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(699, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 48);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(550, 113);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 48);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(623, 263);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(138, 32);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(550, 237);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(270, 20);
+            this.textBox1.TabIndex = 5;
             // 
             // leitor_video2
             // 
@@ -38,13 +97,28 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(864, 403);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.youtube_player);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "leitor_video2";
             this.Text = "Leitor de Vídeo";
+            ((System.ComponentModel.ISupportInitialize)(this.youtube_player)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private AxShockwaveFlashObjects.AxShockwaveFlash youtube_player;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
